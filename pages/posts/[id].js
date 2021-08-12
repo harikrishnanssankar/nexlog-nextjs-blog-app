54f073c96d537b8c8c01abd11b00d47b70d90591
harikrishnanssankar <harikrishnanssankar@gmail.com>
2021-08-12 23:41:37 +0530

    build error bug fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,6 +10,11 @@ export default function Post({ post }) {
       <div>Loading...</div>
     )
   }
+  if (!post) {
+    return (
+      <div>Error</div>
+    )
+  }
   function rectifyFormat(s) {
     let b = s.split(/\D/);
     return b[0] + '-' + b[1] + '-' + b[2] + 'T' +
@@ -20,13 +25,13 @@ export default function Post({ post }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full pt-20">
       <div className="md:max-w-2xl w-11/12 mb-20 ">
-        <h1 className="text-3xl font-semibold tracking-wide">{post?.title}</h1>
+        <h1 className="text-3xl font-semibold tracking-wide">{post.title}</h1>
         <div className="flex items-center justify-between" >
-          <p className="text-sm font-light my-1 ">by {post?.user_email}</p>
+          <p className="text-sm font-light my-1 ">by {post.user_email}</p>
           <p className="text-xs font-light my-1 mr-14 ">Created: {createdAt.toLocaleString('en-IN', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true, day: 'numeric', month: 'numeric', year: 'numeric' })}</p>
         </div>
         <div className="mt-8">
-          <ReactMarkdown className='prose' children={post?.content} />
+          <ReactMarkdown className='prose' children={post.content} />
         </div>
       </div>
       <div className="w-full pb-10">
